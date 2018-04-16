e17ff956db32c041e5a785bdd2bc8a2ec314b9d1
lapots <sebastrident@gmail.com>
2018-04-16 19:14:15 +0300

    VPC config

--- aws/judge-rule-engine/main.tf
+++ aws/judge-rule-engine/main.tf
@@ -9,12 +9,3 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
-
-resource "aws_instance" "judge-rule-engine" {
-  ami = "ami-ac442ac3"
-  instance_type = "t2.micro"
-  tags {
-    Name = "Judge Rule Engine"
-    Environment = "Dev"
-  }
-}
