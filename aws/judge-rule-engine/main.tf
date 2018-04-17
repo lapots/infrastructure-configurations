490bc8c8e542012a3f03df8d69adde0d846bc9a1
lapots <sebastrident@gmail.com>
2018-04-17 16:03:38 +0300

    Removed VPC due to not being free, plain ec2 and rds configuration

--- aws/judge-rule-engine/main.tf
+++ aws/judge-rule-engine/main.tf
@@ -10,4 +10,29 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-# should contain only instance configuration in the future
+resource "aws_db_instance" "default" {
+  allocated_storage = 10
+  storage_type = "gp2"
+  engine = "postgres"
+  instance_class = "db.t2.micro"
+  name = "core"
+  port = 5432
+  engine_version = "10"
+
+  username = "${var.username}"
+  password = "${var.password}"
+}
+
+resource "aws_instance" "default" {
+  ami = "ami-ac442ac3"
+  availability_zone = "eu-central-1a"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  tags {
+    Name = "Rule engine application"
+    Environment = "Dev"
+  }
+}
+
+variable "username" {}
+variable "password" {}
